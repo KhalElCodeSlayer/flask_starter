5a7b960489d70a467cf8f5b67d0505854ed56a3d
khalelcodeslayer <khalidwilliams12@yahoo.com>
2022-03-21 17:20:10 -0500

    Edited on Linux 2

--- app/config.py
+++ app/config.py
@@ -9,8 +9,7 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER','./uploads')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    # ,'').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = True # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
 # class DevelopmentConfig(Config):

baa259348bafedb8f1e17fa5147950526ecda4da
KhalElCodeSlayer <71866354+KhalElCodeSlayer@users.noreply.github.com>
2022-03-20 21:28:50 -0500

    fixing part 1

--- app/config.py
+++ app/config.py
@@ -7,9 +7,10 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER','./uploads')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
-    SQLALCHEMY_TRACK_MODIFICATIONS = True # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    UPLOAD_FOLDER = "./uploads"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://","postgresql://")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 # class DevelopmentConfig(Config):
 #     """Development Config that extends the Base Config Object"""

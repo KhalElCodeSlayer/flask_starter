da68e395261d63be49db556ee75855618a46c0da
KhalElCodeSlayer <71866354+KhalElCodeSlayer@users.noreply.github.com>
2022-03-21 00:49:39 -0500

    modified for heroku

--- app/config.py
+++ app/config.py
@@ -9,9 +9,8 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER','./uploads')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL','').replace('postgres://', 'postgresql://')
     SQLALCHEMY_TRACK_MODIFICATIONS = True # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
-    # conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
 
 # class DevelopmentConfig(Config):
 #     """Development Config that extends the Base Config Object"""

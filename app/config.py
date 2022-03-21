5bd2d29d65ed53f4b56f22fc82e05fd4c7a41766
KhalElCodeSlayer <71866354+KhalElCodeSlayer@users.noreply.github.com>
2022-03-20 23:27:29 -0500

    Trying part 2

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+import psycopg2
 
 load_dotenv()  # load environment variables from .env if it exists.
 
@@ -7,10 +8,10 @@ class Config(object):
     """Base Config Object"""
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'Som3$ec5etK*y')
-    UPLOAD_FOLDER = "./uploads"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("postgres://","postgresql://")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER','./uploads')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://yourusername:yourpassword@localhost/databasename'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
+    # conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
 
 # class DevelopmentConfig(Config):
 #     """Development Config that extends the Base Config Object"""
